9101803978c8e982268dc0e61ec61b8c4b575802
TiffanyH <tiffanyxho@gmail.com>
2018-03-29 22:32:00 -0700

    added comments for useful sources for the next time I work

--- SeniorProjJS.js
+++ SeniorProjJS.js
@@ -1,7 +1,7 @@
-//console.log("hello world");
-// hello world
+// anchor links - may be handy later
 
-/*
+// This link may be handy for solving the below problem: https://stackoverflow.com/questions/1947263/using-an-html-button-to-call-a-javascript-function?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+/* This link was used for the below commented code: https://www.w3schools.com/js/js_htmldom_nodes.asp
 document.getElementById("real_fire_button").addEventListener("click", display_responses);
 
 function display_responses(){
@@ -13,6 +13,7 @@ function display_responses(){
 }
 */
 
+// Should display how many people pressed which button to users
 function display_responses(){
     document.getElementById("real_fire_text").innerHTML = "HIIII!!!";
 }
