0b348726600b15fcfa817051d2a253a5ec4b05f2
TiffanyH <tiffanyxho@gmail.com>
2018-03-29 22:25:27 -0700

    tried to get display_responses function to work two different ways, but only managed to get it to work one way

--- SeniorProjJS.js
+++ SeniorProjJS.js
@@ -1,15 +1,17 @@
 //console.log("hello world");
 // hello world
 
-/*document.getElementById("real_fire_button").addEventListener("click", display_responses());
+/*
+document.getElementById("real_fire_button").addEventListener("click", display_responses);
 
 function display_responses(){
     var para = document.createElement("p");
     var node = document.createTextNode("TEXT:!!!");
     para.appendChild(node);
-    var element = document.getElementsById("end_screen");
+    var element = document.getElementsById("notification_text");
     element.appendChild(para);
-}*/
+}
+*/
 
 function display_responses(){
     document.getElementById("notification_text").innerHTML = "HIIII!!!";
