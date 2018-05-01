a255caba96ed151b7404efe67fd7cbfa5e3d1729
TiffanyH <tiffanyxho@gmail.com>
2018-04-30 22:12:28 -0700

    fixed syntax for posts created

--- SeniorProjServer.py
+++ SeniorProjServer.py
@@ -5,8 +5,8 @@ res = requests.get('htttp://localhost:9200')
 print(res.content)
 """
 
-see_fire = requests.post("http://localhost:9200/buttonPress/button/yesAlarmSeeFire"), json = {"buttonPress": "yesAlarmSeeFire"}
-no_fire = requests.post("http://localhost:9200/buttonPress/button/yesAlarmNoFire"), json = {"buttonPress": "yesAlarmNoFire"}
-no_alarm = requests.post("http://localhost:9200/buttonPress/button/noAlarm"), json = {"buttonPress": "noAlarm"}
-fire_drill = requests.post("http://localhost:9200/buttonPress/button/fireDrill"), json = {"buttonPress": "fireDrill"}
+see_fire = requests.post("http://localhost:9200/button_press/button/yes_alarm_see_fire", json = {"button_press": "yes_alarm_see_fire"})
+no_fire = requests.post("http://localhost:9200/button_press/button/yes_alarm_no_fire", json = {"button_press": "yes_alarm_no_fire"})
+no_alarm = requests.post("http://localhost:9200/button_press/button/no_alarm", json = {"button_press": "no_alarm"})
+fire_drill = requests.post("http://localhost:9200/button_press/button/fire_drill", json = {"button_press": "fire_drill"})
 
