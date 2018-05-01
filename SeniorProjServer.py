5b571730102767087b473cbeb0e1eeb64b3ece1d
TiffanyH <tiffanyxho@gmail.com>
2018-04-30 22:20:25 -0700

    stored all versions of the different request posts into their own variables

--- SeniorProjServer.py
+++ SeniorProjServer.py
@@ -13,8 +13,11 @@ fire_drill = requests.post("http://localhost:9200/button_press/button/fire_drill
 get_see_fire = requests.get("http://localhost:9200/button_press/button/yes_alarm_see_fire")
 see_fire_version = get_see_fire.json()["_version"]
 
-get_no_fire
+get_no_fire = requests.get("http://localhost:9200/button_press/button/yes_alarm_no_fire")
+see_no_fire = get_no_fire.json()["_version"]
 
-get_no_alarm
+get_no_alarm = requests.get("http://localhost:9200/button_press/button/no_alarm")
+see_no_alarm = get_no_alarm.json()["_version"]
 
-get_fire_drill
+get_fire_drill = requests.get("http://localhost:9200/button_press/button/fire_drill")
+see_fire_drill = get_fire_drill.json()["_version"]
