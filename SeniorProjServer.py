fc764d996640835ece2c9a0699bcf5831974aac2
TiffanyH <tiffanyxho@gmail.com>
2018-04-30 22:22:50 -0700

    added documentation

--- SeniorProjServer.py
+++ SeniorProjServer.py
@@ -5,11 +5,13 @@ res = requests.get('htttp://localhost:9200')
 print(res.content)
 """
 
+"""create 1 post for each button in app, 4 total"""
 see_fire = requests.post("http://localhost:9200/button_press/button/yes_alarm_see_fire", json = {"button_press": "yes_alarm_see_fire"})
 no_fire = requests.post("http://localhost:9200/button_press/button/yes_alarm_no_fire", json = {"button_press": "yes_alarm_no_fire"})
 no_alarm = requests.post("http://localhost:9200/button_press/button/no_alarm", json = {"button_press": "no_alarm"})
 fire_drill = requests.post("http://localhost:9200/button_press/button/fire_drill", json = {"button_press": "fire_drill"})
 
+# get the information in each of the posts store each post's version into separate variable for use
 get_see_fire = requests.get("http://localhost:9200/button_press/button/yes_alarm_see_fire")
 see_fire_version = get_see_fire.json()["_version"]
 
