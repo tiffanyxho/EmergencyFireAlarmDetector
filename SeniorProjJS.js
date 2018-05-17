148382a820230b598372d9b457abea1ab28905d8
TiffanyH <tiffanyxho@gmail.com>
2018-05-16 19:25:30 -0700

    deleted unnecessary post functions in JS

--- SeniorProjJS.js
+++ SeniorProjJS.js
@@ -65,65 +65,6 @@ function get(theUrl){
     return xmlHttp.responseText;
 }
 
-function post_to_url(path, params, method) {
-    method = method || "post"; // Set method to post by default, if not specified.
-
-    // The rest of this code assumes you are not using a library.
-    // It can be made less wordy if you use one.
-    var form = document.createElement("form");
-    form.setAttribute("method", method);
-    form.setAttribute("action", path);
-
-    var addField = function( key, value ){
-        var hiddenField = document.createElement("input");
-        hiddenField.setAttribute("type", "hidden");
-        hiddenField.setAttribute("name", key);
-        hiddenField.setAttribute("value", value );
-
-        form.appendChild(hiddenField);
-    }; 
-
-    for(var key in params) {
-        if(params.hasOwnProperty(key)) {
-            if( params[key] instanceof Array ){
-                for(var i = 0; i < params[key].length; i++){
-                    addField( key, params[key][i] )
-                }
-            }
-            else{
-                addField( key, params[key] ); 
-            }
-        }
-    }
-
-    document.body.appendChild(form);
-    form.submit();
-}
-
-function post(path, params, method) {
-    method = method || "post"; // Set method to post by default if not specified.
-
-    // The rest of this code assumes you are not using a library.
-    // It can be made less wordy if you use one.
-    var form = document.createElement("form");
-    form.setAttribute("method", method);
-    form.setAttribute("action", path);
-
-    for(var key in params) {
-        if(params.hasOwnProperty(key)) {
-            var hiddenField = document.createElement("input");
-            hiddenField.setAttribute("type", "hidden");
-            hiddenField.setAttribute("name", key);
-            hiddenField.setAttribute("value", params[key]);
-
-            form.appendChild(hiddenField);
-        }
-    }
-
-    document.body.appendChild(form);
-    form.submit();
-}
-
 /*Attempt at JS see_fire*/
 function see_fire(){
     var see_fire = get("http://localhost:5000/see_fire");
