19e4596990476f58f9116ed648414b75cf6e26aa
TiffanyH <tiffanyxho@gmail.com>
2018-05-19 18:00:16 -0700

    deleted main func and display_responses func

--- SeniorProjJS.js
+++ SeniorProjJS.js
@@ -19,29 +19,6 @@ var real_fire_button_clicks = 0, fire_drill_button_clicks = 0;
 var dont_see_fire_button_clicks = 0, dont_hear_alarm_button = 0;
 var last_button_clicked_id = "";
 
-/* Trying to use an EventListener to increment responses instead of onclick in html
-function main(){
-    document.getElementById("real_fire_text").addEventListener("click", display_responses("real_fire_button"));    
-}
-document.getElementById("real_fire_text").addEventListener("click", display_responses("real_fire_button"));
-*/
-
-function display_responses(id){
-    if (id === "real_fire_button"){
-        real_fire_button_clicks += 1;
-    }else if (id === "fire_drill_button"){
-        fire_drill_button_clicks += 1;
-    }else if (id ==="dont_see_fire_button"){
-        dont_see_fire_button_clicks += 1;
-    }else if (id === "dont_hear_alarm_button"){
-        dont_hear_alarm_button += 1;
-    }
-    last_button_clicked_id = id;
-    document.getElementById("real_fire_text").innerHTML = "Real fire: " + real_fire_button_clicks;
-    document.getElementById("fire_drill_text").innerHTML = "It's a drill: " + fire_drill_button_clicks;
-    document.getElementById("dont_see_fire_text").innerHTML = "Don't see fire: " + dont_see_fire_button_clicks;
-    document.getElementById("dont_hear_alarm_text").innerHTML = "Don't hear alarm: " + dont_hear_alarm_button;
-}
 
 /*Clears the elemnts in a div*/
 function clearBox(elementID){
